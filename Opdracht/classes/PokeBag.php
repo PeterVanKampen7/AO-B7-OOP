522862ce1516dbb183bae8af90caed6a0251690d
Peter <99065921@mydavinci.nl>
2022-03-16 12:46:07 +0100

    Extra

---
+++ Opdracht/classes/PokeBag.php
@@ -0,0 +1,34 @@
+<?php
+
+Class PokeBag {
+
+    private $bag = array();
+
+    public function addToBag($pokemon){
+        if(count($this->bag) <= 10) return;
+        $this->bag[$pokemon->getName()] = $pokemon;
+    }
+
+    public function removeFromBag($name){
+        unset($this->bag[$name]);
+    }
+
+    public function getFromBag($name){
+        return $this->bag[$name];
+    }
+
+    public function getBag(){
+        $return = array();
+        foreach($this->bag as $pokemon){
+            array_push($return, $pokemon->getName());
+        }
+        return $return;
+    }
+
+    public function clearBag(){
+        $this->bag = array();
+    }
+
+}
+
+?>
